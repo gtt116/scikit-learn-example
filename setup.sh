7679eae242425ce1a311093f3a947b420f763fd9
gtt116 <gtt116@gmail.com>
2017-03-12 16:29:10 +0800

    Requirement

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt-get install python-dev libblas-dev liblapack-dev libatlas-base-dev gfortran
+sudo apt-get install python-dev libblas-dev liblapack-dev libatlas-base-dev gfortran libpng-dev libfreetype6-dev
