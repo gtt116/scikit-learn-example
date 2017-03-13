09ed9f08816609c510cd4d664b92da120fbd85df
gtt116 <gtt116@gmail.com>
2017-03-13 23:13:07 +0800

    add digit example

--- digit.py
+++ digit.py
@@ -13,3 +13,5 @@ for i in xrange(64):
     ax = fig.add_subplot(8, 8, i + 1, xticks=[], yticks=[])
     ax.imshow(digits.images[i], cmap=plt.cm.binary, interpolation='nearest')
     ax.text(0, 7, str(digits.target[i]))
+
+plt.show()
